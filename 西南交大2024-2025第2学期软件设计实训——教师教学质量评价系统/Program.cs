d1db8edf81cdcc9e8bdd3a60d7494702a254f2e6
FreOme <2067666131@qq.com>
2025-07-10 20:58:58 +0800

    添加项目文件。

---
+++ 西南交大2024-2025第2学期软件设计实训——教师教学质量评价系统/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 西南交大2024_2025第2学期软件设计实训__教师教学质量评价系统
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
