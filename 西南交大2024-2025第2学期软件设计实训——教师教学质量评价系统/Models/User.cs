8c6d740498816dec4b9f1c5b83267c9a027594bf
FreOme <2067666131@qq.com>
2025-07-10 21:18:54 +0800

    NaN1

---
+++ 西南交大2024-2025第2学期软件设计实训——教师教学质量评价系统/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 西南交大2024_2025第2学期软件设计实训__教师教学质量评价系统.Models
+{
+    [Table("Users")]
+    internal class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
