ded1f681f7b84c2e0def9956b0740864ea89b19a
FreOme <2067666131@qq.com>
2025-07-12 17:03:13 +0800

    menu使用

--- 西南交大2024-2025第2学期软件设计实训——教师教学质量评价系统/UI/AdminMainPage.Designer.cs
+++ 西南交大2024-2025第2学期软件设计实训——教师教学质量评价系统/UI/AdminMainPage.Designer.cs
@@ -28,22 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            AntdUI.MenuItem menuItem40 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem41 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem42 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem43 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem44 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem45 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem46 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem47 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem48 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem49 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem50 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem51 = new AntdUI.MenuItem();
-            AntdUI.MenuItem menuItem52 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem14 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem15 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem16 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem17 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem18 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem19 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem20 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem21 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem22 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem23 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem24 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem25 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem26 = new AntdUI.MenuItem();
             this.pageHeader1 = new AntdUI.PageHeader();
             this.table1 = new AntdUI.Table();
             this.menu1 = new AntdUI.Menu();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // pageHeader1
@@ -59,7 +61,7 @@
             // 
             // table1
             // 
-            this.table1.Location = new System.Drawing.Point(698, 266);
+            this.table1.Location = new System.Drawing.Point(586, 535);
             this.table1.Name = "table1";
             this.table1.Size = new System.Drawing.Size(562, 420);
             this.table1.TabIndex = 1;
@@ -68,41 +70,60 @@
             // menu1
             // 
             this.menu1.Indent = true;
-            menuItem40.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            menuItem41.Text = "查看任务";
-            menuItem42.Text = "发布任务";
-            menuItem40.Sub.Add(menuItem41);
-            menuItem40.Sub.Add(menuItem42);
-            menuItem40.Text = "评价任务管理";
-            menuItem43.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            menuItem44.Text = "全部课程";
-            menuItem45.Text = "选课情况";
-            menuItem43.Sub.Add(menuItem44);
-            menuItem43.Sub.Add(menuItem45);
-            menuItem43.Text = "课程管理";
-            menuItem46.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            menuItem47.Text = "教师";
-            menuItem48.Text = "学生";
-            menuItem49.Text = "督导组";
-            menuItem46.Sub.Add(menuItem47);
-            menuItem46.Sub.Add(menuItem48);
-            menuItem46.Sub.Add(menuItem49);
-            menuItem46.Text = "人员管理";
-            menuItem50.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            menuItem51.Text = "全部评价详情";
-            menuItem52.Text = "教师评分总分";
-            menuItem50.Sub.Add(menuItem51);
-            menuItem50.Sub.Add(menuItem52);
-            menuItem50.Text = "教学质量评价详情";
-            this.menu1.Items.Add(menuItem40);
-            this.menu1.Items.Add(menuItem43);
-            this.menu1.Items.Add(menuItem46);
-            this.menu1.Items.Add(menuItem50);
+            menuItem14.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            menuItem15.Text = "查看任务";
+            menuItem16.Text = "发布任务";
+            menuItem14.Sub.Add(menuItem15);
+            menuItem14.Sub.Add(menuItem16);
+            menuItem14.Text = "评价任务管理";
+            menuItem17.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            menuItem18.Text = "全部课程";
+            menuItem19.Text = "选课情况";
+            menuItem17.Sub.Add(menuItem18);
+            menuItem17.Sub.Add(menuItem19);
+            menuItem17.Text = "课程管理";
+            menuItem20.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            menuItem21.Text = "教师";
+            menuItem22.Text = "学生";
+            menuItem23.Text = "督导组";
+            menuItem20.Sub.Add(menuItem21);
+            menuItem20.Sub.Add(menuItem22);
+            menuItem20.Sub.Add(menuItem23);
+            menuItem20.Text = "人员管理";
+            menuItem24.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            menuItem25.Text = "全部评价详情";
+            menuItem26.Text = "教师评分总分";
+            menuItem24.Sub.Add(menuItem25);
+            menuItem24.Sub.Add(menuItem26);
+            menuItem24.Text = "教学质量评价详情";
+            this.menu1.Items.Add(menuItem14);
+            this.menu1.Items.Add(menuItem17);
+            this.menu1.Items.Add(menuItem20);
+            this.menu1.Items.Add(menuItem24);
             this.menu1.Location = new System.Drawing.Point(12, 100);
             this.menu1.Name = "menu1";
             this.menu1.Size = new System.Drawing.Size(341, 911);
             this.menu1.TabIndex = 3;
             this.menu1.Text = "menu1";
+            this.menu1.SelectChanged += new AntdUI.SelectEventHandler(this.menu1_SelectChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(752, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "显示1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(755, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "显示2";
             // 
             // AdminMainPage
             // 
@@ -110,6 +131,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1452, 1023);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menu1);
             this.Controls.Add(this.table1);
             this.Controls.Add(this.pageHeader1);
@@ -118,6 +141,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AdminMainPage";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -126,5 +150,7 @@
         private AntdUI.PageHeader pageHeader1;
         private AntdUI.Table table1;
         private AntdUI.Menu menu1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- 西南交大2024-2025第2学期软件设计实训——教师教学质量评价系统/UI/AdminMainPage.cs
+++ 西南交大2024-2025第2学期软件设计实训——教师教学质量评价系统/UI/AdminMainPage.cs
@@ -16,5 +16,36 @@ namespace EvaluationSystem.UI
         {
             InitializeComponent();
         }
+
+        private void menu1_SelectChanged(object sender, AntdUI.MenuSelectEventArgs e)
+        {
+            // 修复错误：使用 e.Value 代替 e.SelectedItem
+            var selectedItem = e.Value; // MenuSelectEventArgs 的 Value 属性返回 MenuItem 类型
+            if (selectedItem == null)
+            {
+                MessageBox.Show("未选择任何菜单项，请重试。");
+                return;
+            }
+
+            // 根据菜单选择项加载不同的内容
+            switch (selectedItem.Text)
+            {
+                case "查看任务":
+                    // 加载已评价内容
+                    label1.Show();
+                    label2.Hide();
+                    //LoadEvaluatedTasks();
+                    break;
+                case "发布任务":
+                    // 加载待评价内容
+                    label1.Hide();
+                    label2.Show();
+                    //LoadPendingTasks();
+                    break;
+                default:
+                    MessageBox.Show("未知菜单项，请联系管理员。");
+                    break;
+            }
+        }
     }
 }
